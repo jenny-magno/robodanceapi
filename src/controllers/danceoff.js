654d7e735e1fea56add50d62bffd7ce9f555a3ff
jenny-magno <42797288+jenny-magno@users.noreply.github.com>
2020-03-02 12:03:17 +0800

    Updated error responses

--- src/controllers/danceoff.js
+++ src/controllers/danceoff.js
@@ -7,7 +7,7 @@ module.exports.holdDanceoff = async(req, res) => {
     const result = await danceoffService.holdDanceoff(req.body.teams);
     res.status(201).json(result);
   } catch (e) {
-    res.status(e.status || 500).send(e.data() || e);
+    res.status(e.status || 500).send(typeof e.data === 'function' ? e.data() : e);
   }
 };
 
@@ -18,7 +18,7 @@ module.exports.findAllDanceoffs = async(req, res) => {
 
     res.json(result);
   } catch (e) {
-    res.status(e.status || 500).send(e.data() || e);
+    res.status(e.status || 500).send(typeof e.data === 'function' ? e.data() : e);
   }
 };
 
@@ -26,10 +26,12 @@ module.exports.findDanceoffsByRobot = async(req, res) => {
   try {
     const robotId = req.params.id;
     const result = await danceoffService.findByRobotId(robotId);
-
+    if (result == null) {
+      return res.status(404).end();
+    }
     res.json(result);
   } catch (e) {
-    res.status(e.status || 500).send(e.data() || e);
+    res.status(e.status || 500).send(typeof e.data === 'function' ? e.data() : e);
   }
 };
 
